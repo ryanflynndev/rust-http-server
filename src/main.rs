ce41b94cb1d61aec99dee526e813d97729e8ad47
Ryan Flynn <ryanflynn44@gmail.com>
2021-11-15 10:34:32 -0500

    Built the address

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
+    let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
@@ -16,6 +16,6 @@ impl Server {
     }
 
     fn run(self) {
-
+        println!("Listening on {}", self.address)
     }
 }
