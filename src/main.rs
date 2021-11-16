32d7fbfee7a55219acdfbe16d2723c5f5161e7b3
Ryan Flynn <ryanflynn44@gmail.com>
2021-11-15 21:09:16 -0500

    Added method enum

--- src/main.rs
+++ src/main.rs
@@ -19,3 +19,21 @@ impl Server {
         println!("Listening on {}", self.address)
     }
 }
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
