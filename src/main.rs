841f61d0f83c3e6b14b063a4bc00bbfe47d3d2a7
Ryan Flynn <ryanflynn44@gmail.com>
2021-11-15 22:00:07 -0500

    Reorganized everything into modules

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,11 @@
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod server;
+mod http;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-
-struct Server {
-    address: String,
-}
-
-impl Server {
-    fn new(address: String) -> Self {
-        Self {
-            address
-        }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.address)
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
 }
